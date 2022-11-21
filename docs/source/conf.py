16b531c757c196cccef0cc7bc817ffc91f60dfd4
Stephan Linz <linz@li-pro.net>
2022-11-21 23:31:58 +0100

    docs: enable auto-documentation extensions
    - sphinx.ext.autodoc
    - sphinx.ext.autosummary
    - sphinx.ext.coverage
    - sphinx.ext.doctest
    - sphinx.ext.napoleon
    - sphinx.ext.viewcode
    
    For details, see:
    
    - https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
    - https://www.sphinx-doc.org/en/master/usage/extensions/autosummary.html
    - https://www.sphinx-doc.org/en/master/usage/extensions/coverage.html
    - https://www.sphinx-doc.org/en/master/usage/extensions/doctest.html
    - https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
    - https://www.sphinx-doc.org/en/master/usage/extensions/viewcode.html
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src"))
 
 
 # -- Project information -----------------------------------------------------
@@ -28,8 +29,14 @@ author = "TiaC Systems"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
     "sphinx.ext.autosectionlabel",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.napoleon",
     "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
     "myst_parser",
 ]
 
